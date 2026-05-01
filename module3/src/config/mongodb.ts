2d782571d952ba9766340d0de0414837e8cfc8f8
Lenovo <jannatjui414@gmail.com>
2026-05-01 09:57:48 +0600

    add env file for mondodb crediential

--- module3/src/config/mongodb.ts
+++ module3/src/config/mongodb.ts
@@ -1,7 +1,7 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
-
-const uri =
-  "mongodb+srv://todos-user:yyqpAwLTSbPhZRoy@cluster0.jwr0f.mongodb.net/todosDB?appName=Cluster0";
+import dotenv from "dotenv";
+dotenv.config();
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jwr0f.mongodb.net/${process.env.DB_NAME}?appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 export const client = new MongoClient(uri, {
   serverApi: {
